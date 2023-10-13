1e60b9c0d135d0701d273fd25ac78de73ffcbd9e
stilletto <avatarnarod@gmail.com>
2023-10-13 19:32:28 +0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
+
 setup(
     name='roop',
     version='0.1.0',
     packages=['roop', 'roop.processors', 'roop.processors.frame'],
     url='',
     license='',
-    author='A',
+    author='avata',
     author_email='',
-    description='',
+    description=''
 )
