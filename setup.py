205287c52f6f1dbe3fb5321706e1b15e14d2a7c7
stilletto <avatarnarod@gmail.com>
2023-10-13 18:49:29 +0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 setup(
     name='roop',
     version='0.1.0',
     packages=['roop', 'roop.processors', 'roop.processors.frame'],
     url='',
     license='',
-    author='avata',
+    author='A',
     author_email='',
-    description=''
+    description='',
+    install_requires=required,
 )
